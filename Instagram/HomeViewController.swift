44a90e55516e19ad36bfce4ba6827650e0fc57aa
emi <bluming_blue@icloud.com>
2022-03-24 05:52:40 +0900

    コードを教えていただいた通りに変更しましたが、まだエラーが出ています。

--- Instagram/HomeViewController.swift
+++ Instagram/HomeViewController.swift
@@ -14,102 +14,63 @@ class HomeViewController: UIViewController,UITableViewDataSource, UITableViewDel
     
     var postArray: [PostData] = []
     
-    var listener: ListenerRegistration?
+    var listener: ListenerRegistration!
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        let nib = UINib(nibName: "PostTableViewCell", bundle: nil)
-        tableView.register(nib, forCellReuseIdentifier: "Cell")
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
+           super.viewDidLoad()
+
+           tableView.delegate = self
+           tableView.dataSource = self
+
+           // カスタムセルを登録する
+           let nib = UINib(nibName: "PostTableViewCell", bundle: nil)
+           tableView.register(nib, forCellReuseIdentifier: "Cell")
+       }
+
+       override func viewWillAppear(_ animated: Bool) {
            super.viewWillAppear(animated)
            print("DEBUG_PRINT: viewWillAppear")
-        
+           // ログイン済みか確認
            if Auth.auth().currentUser != nil {
                
            let postsRef = Firestore.firestore().collection(Const.PostPath).order(by: "date", descending: true)
                listener = postsRef.addSnapshotListener() { (querySnapshot, error) in
-           if let error = error {
-                  print("DEBUG_PRINT: snapshotの取得が失敗しました。 \(error)")
-                  return
-           }
-                   
+            if let error = error {
+            print("DEBUG_PRINT: snapshotの取得が失敗しました。 \(error)")
+            return
+            }
+                         // 取得したdocumentをもとにPostDataを作成し、postArrayの配列にする。
             self.postArray = querySnapshot!.documents.map { document in
-                  print("DEBUG_PRINT: document取得 \(document.documentID)")
-              let postData = PostData(document: document)
-              return postData
+            print("DEBUG_PRINT: document取得 \(document.documentID)")
+            let postData = PostData(document: document)
+            return postData
             }
-                   
+                         // TableViewの表示を更新する
             self.tableView.reloadData()
-            }
-          }
-     }
-
-    override func viewWillDisappear(_ animated: Bool) {
-           super.viewWillDisappear(animated)
-           print("DEBUG_PRINT: viewWillDisappear")
-           listener?.remove()
-       }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return postArray.count
-        }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PostTableViewCell
-            cell.setPostData(postArray[indexPath.row])
-        
-        cell.likeButton.addTarget(self, action:#selector(handleButton(_:forEvent:)), for: .touchUpInside)
-        
-        cell.comentWriteButton.addTarget(self, action:#selector(touchWriteButton(_:forEvent:)), for: .touchUpInside)
-
-        return cell
-    }
-    
-    @objc func touchWriteButton(_ sender: UIButton, forEvent event: UIEvent){
-        
-        // タップされたセルのインデックスを求める
-        let touch = event.allTouches?.first
-        let point = touch!.location(in: self.tableView)
-        let indexPath = tableView.indexPathForRow(at: point)
-
-            // 配列からタップされたインデックスのデータを取り出す
-        let postData = postArray[indexPath!.row]
-        let comentViewController = self.storyboard?.instantiateViewController(withIdentifier: "comentView") as! ComentWriteViewController
-        comentViewController.id = postData.id
-        self.present(comentViewController, animated: true, completion: nil)
-    }
-    
-    @objc func handleButton(_ sender: UIButton, forEvent event: UIEvent) {
-            print("DEBUG_PRINT: likeボタンがタップされました。")
-        
-            let touch = event.allTouches?.first
-            let point = touch!.location(in: self.tableView)
-            let indexPath = tableView.indexPathForRow(at: point)
-        
-            let postData = postArray[indexPath!.row]
-        
-            if let myid = Auth.auth().currentUser?.uid {
-                
-                var updateValue: FieldValue
-                if postData.isLiked {
-                    
-                    updateValue = FieldValue.arrayRemove([myid])
-                } else {
-                    
-                    updateValue = FieldValue.arrayUnion([myid])
                 }
-                
-                let postRef = Firestore.firestore().collection(Const.PostPath).document(postData.id)
-                postRef.updateData(["likes": updateValue])
             }
-        }
+            } else {
+                     // ログイン未(またはログアウト済み)
+              if listener != nil {
+                       // listener登録済みなら削除してpostArrayをクリアする
+                 listener.remove()
+                 listener = nil
+                 postArray = []
+                 tableView.reloadData()
+            }
+       }
+
+       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return postArray.count
+       }
+
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           // セルを取得してデータを設定する
+           let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! PostTableViewCell
+           cell.setPostData(postArray[indexPath.row])
+
+           return cell
+       }
     
     
     
